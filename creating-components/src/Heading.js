4b6345035b9607d790ad6ea47cd6eae908c5dafe
Ali Ashraf <mmaashraf798@gmail.com>
2023-09-11 23:54:09 -0700

    Progress Update: Completed React Components and Where they live. Added the updated notion notes. That were missed in previous MR.

---
+++ creating-components/src/Heading.js
@@ -0,0 +1,8 @@
+function  Heading(){
+    return (
+    <h1>Hello world!! This is Ashraf! <br></br>  This is the Header react functional component. <br></br>
+    If you can see this, it means that the export and import statement are successfull.</h1>
+    );
+}
+
+export default Heading;
