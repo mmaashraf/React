fab984cfed4aa45d41bfc154261a2e2dbe4e99d6
Ali Ashraf <mmaashraf798@gmail.com>
2023-09-11 19:17:10 -0700

    Progress update: Added code to a new react functional component and render it in App component.

--- creating-components/src/App.js
+++ creating-components/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+function  Header(){
+  return <h1> Hello world!! This is Ashraf! <br></br>  This is the Header react functional component</h1>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <div className="App"> 
+      This is the starting code for 'App' react component. Below is the Header component.
+      <Header/> 
+    </div> 
+  ); 
 }
 
 export default App;
